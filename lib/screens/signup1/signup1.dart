1ca5412968479b80f6b7af36d8103b46a181561d
AlanGtz59 <alanminjarez59@gmail.com>
2024-06-28 11:15:14 -0600

    Se agrego el boton de ayuda, y se añadio un pequeño texto que explica donde encontrarlo, ademas que se creo el widget inkbutton

--- lib/screens/signup1/signup1.dart
+++ lib/screens/signup1/signup1.dart
@@ -1,4 +1,5 @@
 import 'package:auto_matic/config/config.dart';
+import 'package:auto_matic/screens/signup1/widgets/ink_button.dart';
 
 class SinguUp1 extends StatelessWidget {
   const SinguUp1({super.key});
@@ -8,7 +9,7 @@ class SinguUp1 extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
@@ -20,32 +21,16 @@ class SinguUp1 extends StatelessWidget {
                   fontSize: 24,
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: IconButton(
-                  icon: Icon(Icons.question_mark, color: Colors.black),
-                  onPressed: () {
-                    // Lógica para mostrar texto cuando se presiona el botón de pregunta
-                    showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        content: Text('Suele estar en la caja o etiqueta del producto, junto al código de barras. Tiene entre 8 y 14 dígitos.No tiene letras, excepto en autopartes.No es un código interno como el SKU o el código de distribución.Pueden ser de varios tipos como:UPC: es el más común en Estado Unidos y Canadá y tiene 12 dígitos.EAN: es el más común en Europa y tiene 13 dígitos.JAN: es el más común en Japón y tiene 13 dígitos.ISBN: se utiliza principalmente para identificar libros.'),
-                      ),
-                    );
-                  },
-                ),
-              ),
+              InkButton(),
             ],
           ),
-          Align(
-            alignment: Alignment.topLeft,
-            child: ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'XXXX-XXXX-XXXX-XXXX',
-                  border: OutlineInputBorder(), // Agregamos borde al TextField
-                ),
+          ConstrainedBox(
+            constraints:
+                BoxConstraints(maxWidth: MediaQuery.of(context).size.width),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: 'XXXX-XXXX-XXXX-XXXX',
+                border: OutlineInputBorder(), // Agregamos borde al TextField
               ),
             ),
           ),
@@ -53,7 +38,8 @@ class SinguUp1 extends StatelessWidget {
             onPressed: () {
               // Lógica para el botón "Continuar"
             },
-            child: Text('Continuar', style: TextStyle(color: Config.firstColor)),
+            child:
+                Text('Continuar', style: TextStyle(color: Config.firstColor)),
             style: ElevatedButton.styleFrom(
               backgroundColor: Config.sixColor,
             ),
