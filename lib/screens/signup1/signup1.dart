4474c957d6395be2e5f2e112149df32f86741d4d
AlanGtz59 <alanminjarez59@gmail.com>
2024-06-28 10:21:22 -0600

    Se agrego el boton de ayuda, y se añadio un pequeño texto que explica donde encontrarlo.

--- lib/screens/signup1/signup1.dart
+++ lib/screens/signup1/signup1.dart
@@ -6,18 +6,45 @@ class SinguUp1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(10.0),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween, // Alinear al principio
-        crossAxisAlignment: CrossAxisAlignment.start, // Alinear a la izquierda
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Align( // Alinear el TextField a la esquina superior izquierda
+          Row(
+            children: [
+              Text(
+                'Introduce el código único del producto',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: IconButton(
+                  icon: Icon(Icons.question_mark, color: Colors.black),
+                  onPressed: () {
+                    // Lógica para mostrar texto cuando se presiona el botón de pregunta
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        content: Text('Suele estar en la caja o etiqueta del producto, junto al código de barras. Tiene entre 8 y 14 dígitos.No tiene letras, excepto en autopartes.No es un código interno como el SKU o el código de distribución.Pueden ser de varios tipos como:UPC: es el más común en Estado Unidos y Canadá y tiene 12 dígitos.EAN: es el más común en Europa y tiene 13 dígitos.JAN: es el más común en Japón y tiene 13 dígitos.ISBN: se utiliza principalmente para identificar libros.'),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+          Align(
             alignment: Alignment.topLeft,
-            child: ConstrainedBox( // Limitar el ancho del TextField
-              constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width ),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width),
               child: TextFormField(
                 decoration: InputDecoration(
-                  labelText: 'Introduce el código único del producto',
+                  labelText: 'XXXX-XXXX-XXXX-XXXX',
+                  border: OutlineInputBorder(), // Agregamos borde al TextField
                 ),
               ),
             ),
@@ -26,16 +53,12 @@ class SinguUp1 extends StatelessWidget {
             onPressed: () {
               // Lógica para el botón "Continuar"
             },
-            child: Text('Continuar', style: TextStyle(color: Config.firstColor)), // Letra blanca
+            child: Text('Continuar', style: TextStyle(color: Config.firstColor)),
             style: ElevatedButton.styleFrom(
-              backgroundColor: Config.sixColor, // Color hexadecimal #208454
+              backgroundColor: Config.sixColor,
             ),
           ),
-          SizedBox(height: 32.0),
           TimelineWidget(currentPage: 0), // Línea de tiempo
-          SizedBox(height: 16.0),
-
-          SizedBox(height: 16.0),
         ],
       ),
     );
