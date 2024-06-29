04db737159875331e89370df69ef7783e50ed899
carmain93 <137136553+carmain93@users.noreply.github.com>
2024-06-28 20:55:24 -0600

    correccion segunda pantalla
    se termino la segunda pantalla

--- lib/screens/signup3/widgets/datebox.dart
+++ lib/screens/signup3/widgets/datebox.dart
@@ -11,9 +11,20 @@ class _DateboxState extends State<Datebox> {
   @override
   Widget build(BuildContext context) {
     return Card(
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: Colors.black, width: 2.0), // Define el borde de la card
+        borderRadius: BorderRadius.circular(4.0), // Define el radio de los bordes
+      ),
       child: Column(
         children: [
-          Row( children: [Text("data")],)
+          Row( children: [Text("Modelo"), SizedBox(width: 50,),Text("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")],),
+          SizedBox(height: 20,),
+          Row(children: [Text("Fabricante"),SizedBox(width: 50,),Text("xxxxxxxxxxxxxxxxxxxxxxxxxxxx")],),
+          SizedBox(height: 20,),
+          Row(children: [Text("Año"),SizedBox(width: 50,),Text("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")],),
+          SizedBox(height: 20,),
+          Row(children: [Text("VIM"),SizedBox(width: 50,),Text("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")],),
+
         ],
       ),
     );
