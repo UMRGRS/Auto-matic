0a7761cee3badef2aae54315d168eeee07dc2d8d
Alejandro <alejandro.borjon@protonmail.com>
2024-07-05 10:38:51 -0600

    Responsibidad de landing listo, cambian los  row por columns en la card principal y las cards del fondo de la pantalla.

--- lib/screens/landing/widgets/landingcard.dart
+++ lib/screens/landing/widgets/landingcard.dart
@@ -18,32 +18,30 @@ class Landingcard extends StatefulWidget {
 class _LandingcardState extends State<Landingcard> {
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox( constraints: const BoxConstraints(maxHeight: 150),
-      child: Card.filled(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-              side: const BorderSide(color: Colors.black, width: 2.0)),
-          color: const Color(0xff998fc7),
-          child: ListTile(
-              leading: SvgPicture.asset(widget.imagen, height: 50,),
-              title: ConstrainedBox(
-                constraints: const BoxConstraints(minHeight: 120),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      widget.titulo,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      widget.texto,
-                      style: const TextStyle(fontSize: 15),
-                    ),
-                  ],
-                ),
+    return Card.filled(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            side: const BorderSide(color: Colors.black, width: 2.0)),
+        color: const Color(0xff998fc7),
+        child: ListTile(
+            leading: SvgPicture.asset(widget.imagen, height: 50,),
+            title: ConstrainedBox(
+              constraints: const BoxConstraints(minHeight: 120),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    widget.titulo,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    widget.texto,
+                    style: const TextStyle(fontSize: 15),
+                  ),
+                ],
               ),
-          )),
-    );
+            ),
+        ));
   }
 }
