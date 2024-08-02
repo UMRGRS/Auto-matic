7e9b13ef55966de93bf9d37d48e38d26f73f4d61
UMRGRS <115056186+UMRGRS@users.noreply.github.com>
2024-08-02 15:22:29 -0600

    Update delete_car_pop_up.dart

--- lib/app/UI/screens/vehicle_RD/widgets/delete_car_pop_up.dart
+++ lib/app/UI/screens/vehicle_RD/widgets/delete_car_pop_up.dart
@@ -1,7 +1,6 @@
 import 'package:auto_matic/app/UI/screens/vehicle_RD/utils/delete_car_documents.dart';
 import 'package:auto_matic/app/config/config.dart';
 
-//Document this
 Future<void> showDeleteCarPopUp(
     BuildContext context, Map<String, dynamic> references) {
   return showDialog(
@@ -15,13 +14,11 @@ Future<void> showDeleteCarPopUp(
       actions: [
         AppBarButton(
           text: "Confirmar",
-          //Document this
           onPressed: () => deleteCarDocuments(context, references),
           color: Config.confirmGreen,
         ),
         AppBarButton(
           text: "Cancelar",
-          //Document this
           onPressed: () => context.pop(),
           color: Config.fifthColor,
         ),
