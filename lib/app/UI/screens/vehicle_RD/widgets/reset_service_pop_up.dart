61836391eb9ba9eb1c2f501fa2acdf1ca1b8ef18
UMRGRS <115056186+UMRGRS@users.noreply.github.com>
2024-08-02 15:35:10 -0600

    Update reset_service_pop_up.dart

--- lib/app/UI/screens/vehicle_RD/widgets/reset_service_pop_up.dart
+++ lib/app/UI/screens/vehicle_RD/widgets/reset_service_pop_up.dart
@@ -2,7 +2,6 @@ import 'package:auto_matic/app/UI/screens/vehicle_RD/utils/reset_service.dart';
 import 'package:auto_matic/app/config/config.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-//Document this
 Future<void> showResetServicePopUp(
     BuildContext context, DocumentReference reference) {
   return showDialog(
@@ -19,13 +18,11 @@ Future<void> showResetServicePopUp(
       actions: [
         AppBarButton(
           text: "Confirmar",
-          //Document this
           onPressed: () => resetService(context, reference),
           color: Config.confirmGreen,
         ),
         AppBarButton(
           text: "Cancelar",
-          //Document this
           onPressed: () => context.pop(),
           color: Config.fifthColor,
         ),
